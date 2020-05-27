65da6cfd3f2426dfa4324c3388280e392e5bc450
Gedihared6409 <gediali019@gmail.com>
2020-05-27 13:35:08 +0300

    added url for saerch

--- photo/urls.py
+++ photo/urls.py
@@ -5,5 +5,9 @@ from . import views
 
 urlpatterns=[
     url(r'^$',views.index,name='index'),
-  
+    url(r'^location/(\d+)',views.display_location,name='displayLocation'),
+    url(r'^search/',views.search_category,name='search_category'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
