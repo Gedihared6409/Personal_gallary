78612421a1adb39244dc30ea91d2ff3d59cd62a3
Gedihared6409 <gediali019@gmail.com>
2020-05-27 13:24:24 +0300

    added a display location function and added a htnml in lacation.html to display images

--- photo/views.py
+++ photo/views.py
@@ -8,3 +8,11 @@ def index(request):
     locations = Location.objects.all()
     return render(request,'index.html',{'images':images,'locations':locations})
 
+def display_location(request,location_id):
+    try:
+        location = Location.objects.get(id = location_id)
+        images = Image.objects.filter(image_location = location.id)
+        locations = Location.objects.all()
+    except:
+        raise Http404()
+    return render(request,'location.html',{'location':location,'images':images, 'locations':locations})
